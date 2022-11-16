bc0f0b2ef216f17636f2c9824bc251f094973ff1
jcp-qcu <jc.pablico07081998@gmail.com>
2022-11-16 23:12:58 +0800

    Paypal Integration

--- src/app/pricing/pricing.component.ts
+++ src/app/pricing/pricing.component.ts
@@ -1,15 +1,26 @@
 import { Component, OnInit } from '@angular/core';
+import { render } from 'creditcardpayments/creditCardPayments';
 
 @Component({
   selector: 'app-pricing',
   templateUrl: './pricing.component.html',
-  styleUrls: ['./pricing.component.scss']
+  styleUrls: ['./pricing.component.scss'],
 })
 export class PricingComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor() {
+    render({
+      id: '#myPaypalButtons20',
+      currency: 'USD',
+      value: '20.00',
+      onApprove: (details) => {},
+    });
+    render({
+      id: '#myPaypalButtons60',
+      currency: 'USD',
+      value: '60.00',
+      onApprove: (details) => {},
+    });
   }
 
+  ngOnInit(): void {}
 }
